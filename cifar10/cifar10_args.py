cdcab1403b370b90d1c8768fe30595d62175079c
gody7334 <gody7334@gmail.com>
2017-05-23 14:01:28 +0000

    first commit

---
+++ cifar10/cifar10_args.py
@@ -0,0 +1,20 @@
+import sys, argparse
+
+parser = argparse.ArgumentParser(description='train cnn args.')
+parser.add_argument('-f')
+parser.add_argument('-train_dir', help="Directory where to write event logs and checkpoint.", default="/tmp/cifar10_train")
+parser.add_argument('-data_dir', help="Path to the CIFAR-10 data directory.", default="/tmp/cifar10_data")
+parser.add_argument('-max_steps', help="Number of batches to run.", default=10000000,type=int)
+parser.add_argument('-batch_size', help="Number of images to process in a batch.", default=128,type=int)
+parser.add_argument('-log_device_placement', help="Whether to log device placement.", default=False,type=bool)
+parser.add_argument('-use_fp16', help="Train the model using fp16.", default=False,type=bool)
+parser.add_argument('-log_frequency', help="How often to log results to the console.", default=10,type=int)
+
+parser.add_argument('-eval_dir', help="Directory where to write event logs.", default="/tmp/cifar10_eval")
+parser.add_argument('-eval_data', help="Either 'test' or 'train_eval'.", default="test")
+parser.add_argument('-checkpoint_dir', help="Directory where to read model checkpoints.", default="/tmp/cifar10_train")
+parser.add_argument('-eval_interval_secs', help="How often to run the eval.", default=60,type=int)
+parser.add_argument('-num_examples', help="Number of examples to run.", default=1000,type=int)
+parser.add_argument('-run_once', help="Whether to run eval only once.", default=False,type=bool)
+
+args = parser.parse_args()
